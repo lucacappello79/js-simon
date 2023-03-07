86401f02054dc0625e5e0171df0d86ad831d9468
lucacappello79 <postt79@gmail.com>
2023-03-07 17:31:23 +0100

    feat: comparing results work in progress

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 let wrapperEl = document.getElementById("numbers-wrapper")
 let userNumbersEl = document.getElementById("numbers-user")
+let buttonEl = document.getElementById("compare")
+//let enteredNumbers = document.querySelectorAll
 let totalNumbers = 5
 
 
@@ -15,8 +17,22 @@ setTimeout(function() {userNumbersEl.style.display = "flex"}, 10000)
 const totalInputs = generateInputs(totalNumbers);
 userNumbersEl.append(...totalInputs);
 
+buttonEl.addEventListener("click", function(){
+
+    let guessedNumbers = [];
+    for (let i = 0; i < totalInputs.length; i++){
+        
+        let userNumber = Number(totalInputs[i].value);
+
+        guessedNumbers.push(userNumber)
+    }
+
+    let rightGuesses = getRightGuesses(guessedNumbers, numbers);
+    alert("You guessed " + rightGuesses.length + " numbers: " + rightGuesses);
 
 
+
+})
 
 //..........functions
 
@@ -64,5 +80,24 @@ function generateInputs(totalNumbers) {
     }
 
     return totalInputs;
+
+  }
+
+  function getRightGuesses (guessedNumbers, numbers) {
+
+    let rightGuesses = []
+
+    for (let i = 0; i < guessedNumbers.length; i++) {
+
+        let userNumber = guessedNumbers[i];
+
+        if (numbers.includes(userNumber)) {
+
+            rightGuesses.push(userNumber);
+        }
+
+    }
+
+    return rightGuesses;
 
   }
