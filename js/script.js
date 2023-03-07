552bf292e9010cff69207d05a529e0d2d923d3f1
lucacappello79 <postt79@gmail.com>
2023-03-07 14:04:52 +0100

    init exercise

---
+++ js/script.js
@@ -0,0 +1,14 @@
+let wrapperEl = document.getElementById("numbers-wrapper")
+let userNumbersEl = document.getElementById("numbers-user")
+
+function generateNumbers() {
+
+    let number = document.createElement("span")
+    const numbers = [];
+    for (let i = 0; i < 5; i++) {
+
+	numbers.push(Math.floor(Math.random() * 100) + 1);
+    
+}
+
+}
